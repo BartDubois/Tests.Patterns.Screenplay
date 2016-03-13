3af438e18ea01265fd1589b902696ff74e5ec6c0
Bartłomiej Dubas <dubcio@o2.pl>
2016-03-13 01:58:18 +0100

    Actor named John can perform the operation using a tool

---
+++ Tests.Patterns.Screenplay.UnitTests/Class1.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Patterns.Screenplay.UnitTests
+{
+    [TestFixture]
+    public class ActorCan
+    {
+        [Test]
+        // 1. Actor named John can perform the operation using a tool
+        // 2. Actor, as John, can perform the operation using a tool
+        public void ActorNamedJohnCanPerformTheOperationUsingATool()
+        {
+            Actor.Named("John")
+                .Can(PerformOperation.Using(A.Tool()));
+        }
+
+        static class PerformOperation
+        {
+            public static Perform Using(object tool)
+            {
+                return null; 
+            }
+        }
+
+        static class A
+        {
+            public static object Tool()
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ Tests.Patterns.Screenplay/Actor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Patterns.Screenplay
+{
+    // TBD: Does the Serenity BDD implemntation valiotates single rsposibility prinicle [SRP]?
+    public class Actor
+    {
+        // TBD: [SRP] Domain?
+        protected Actor(string name)
+        {
+            _name = name;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+        }
+        private readonly string _name;
+
+        // TBD: [SRP] Fluent?
+        // 2. public static Actor As(string role) - it can be feather used to assign some bunch of allowed operations
+        public static Actor Named(string withName)
+        {
+            return new Actor(withName);
+        }
+
+        public Actor Can(Perform operation)
+        {
+            return this;
+        }
+    }
+}
